6117714ed8cc34460f199fc21cff8f71c792c930
Juanmaa021 <jmardilesj@gmail.com>
2022-04-23 19:28:03 -0400

    Stage 3

--- src/Lamp.java
+++ src/Lamp.java
@@ -36,6 +36,51 @@ public class Lamp extends DomoticDevice{
         OFF
     }
 
+    public  void changeRGB(String RGB){
+        if (RGB == "RU") {
+            if (this.r > 245) {
+                this.r = 256 ;
+            } else {
+                this.r = (short) (this.r + 10);
+            }
+        }
+        if (RGB == "") {
+            if (this.r < 10) {
+                this.r = (0);
+            } else {
+                this.r = (this.r - 10);
+            }
+        }
+        if (RGB == 3) {
+            if (this.b > 245) {
+                this.b = 256 ;
+            } else {
+                this.b = (this.b + 10);
+            }
+        }
+        if (RGB == 4) {
+            if (this.b < 10) {
+                this.b = (0);
+            } else {
+                this.b = (this.b - 10);
+            }
+        }
+        if (RGB == 5) {
+            if (this.g > 245) {
+                this.g = 256 ;
+            } else {
+                this.g = (this.g + 10);
+            }
+        }
+        if (RGB == 6) {
+            if (this.g < 10) {
+                this.g = 0;
+            } else {
+                this.g = (this.g - 10);
+            }
+        }
+    }
+
     private int channel;
     private short r,g,b;
     private LampState state;
